918e9e02458aeec02bba1356eedcb97c8f12eb74
kayelyn08@gmail.com <kayelyn08@gmail.com>
2021-02-14 18:03:59 +0800

    Add Swagger UI

--- src/main/java/com/converter/XMLToJSONConverter/controller/FileUploadController.java
+++ src/main/java/com/converter/XMLToJSONConverter/controller/FileUploadController.java
@@ -3,18 +3,18 @@ package com.converter.XMLToJSONConverter.controller;
 import java.io.IOException;
 import java.util.stream.Collectors;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.models.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -23,6 +23,7 @@ import com.converter.XMLToJSONConverter.exception.StorageFileNotFoundException;
 import com.converter.XMLToJSONConverter.service.StorageService;
 
 @Controller
+@Api(produces = "application/json", value = "Documentation for xml-json converter")
 public class FileUploadController {
 
     private final StorageService storageService;
@@ -53,6 +54,14 @@ public class FileUploadController {
     }
 
     @PostMapping("/")
+    @ApiOperation(value = "Convert XML to JSON", response = ResponseEntity.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 500, message = "Application failed to process the request")
+    })
     public String convertXMLToJSON(@RequestParam("file") MultipartFile file,
                                    RedirectAttributes redirectAttributes) {
 
